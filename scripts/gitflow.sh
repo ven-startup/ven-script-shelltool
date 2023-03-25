aadafc7bcf82e47e3fe14a0ea084714fd56be611
Anthony Bravo Berrio <anthony.bravo.ven@gmail.com>
2023-03-24 20:36:53 -0500

    feat: change validate of branch name

--- scripts/gitflow.sh
+++ scripts/gitflow.sh
@@ -4,7 +4,7 @@ script_gitflow_validate_branch_name() {
   echo -e "\e[32m[INFO]\e[0m Run script_gitflow_validate_branch_name"
   echo -e "\e[38;5;208m[DEBUG]\e[0m Branch name: $GITHUB_HEAD_REF"
 
-  if [[ $GITHUB_HEAD_REF =~ ^refs/heads/feature/.* ]]; then
+  if [[ $GITHUB_HEAD_REF =~ ^feature/.* ]]; then
     echo -e "\e[32m[INFO]\e[0m Correct branch name validation"
   else
     echo -e "\e[31m[ERROR]\e[0m Incorrect branch name validation"
